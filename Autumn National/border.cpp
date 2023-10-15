9bb2c79e0fafef5af617a36a1d4bd24b19390c4e
MateyApostolov <mateyapostolov@gmail.com>
2023-10-15 20:14:47 +0300

    Autumn National/border.cpp

--- Autumn National/border.cpp
+++ Autumn National/border.cpp
@@ -4,12 +4,26 @@ using namespace std;
 int main () {
     int a, b, k;
     cin >> a >> b >> k;
-    int p = (a + b) * 2;
-    int l = p / k;
-    if(p % k != 0) {
-        l++;
+    int z = (a / k) * 2 + (b / k) * 2;
+    int oa = a % k;
+    int ob = b % k;
+    if(oa == 0 && ob == 0) {
+        cout << z;
+        return 0;
     }
-    cout << l;
+    if(oa * 2 + ob * 2 <= k) {
+        cout << z + 1;
+        return 0;
+    }
+    if(min(oa, ob) > 1.0 * k / 2) {
+        cout << z + 4;
+        return 0;
+    }
+    if(oa + ob <= k) {
+        cout << z + 2;
+        return 0;
+    }
+    cout << z + 3;
 
     return 0;
 }
