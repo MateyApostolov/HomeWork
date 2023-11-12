7d3beb4d6900226ece5d574501e3c4d71d7e0476
MateyApostolov <mateyapostolov@gmail.com>
2023-11-12 21:35:48 +0200

    noi2 2009 row

--- NOI1/row.cpp
+++ NOI1/row.cpp
@@ -4,28 +4,19 @@ using namespace std;
 int main () {
     int n;
     cin >> n;
-    pair <int, int> a[n];
+    int a[n], nm = 10000, ng = 0;
     for(int i = 0; i < n; i++) {
-        cin >> a[i].first;
-        a[i].second = i + 1;
+        cin >> a[i];
+        if(nm > a[i]) nm = a[i];
+        if(ng < a[i]) ng = a[i];
     }
-    sort(a, a + n);
-    int ogr = 1.0 * a[0].first / 3 + 1.0 * a[n - 1].first / 3 * 2;
-    int nm = 100000;
-    pair<int, int> h[1];
-    int r;
-    for(int k = 0; k < n; k++) {
-        if(a[k].first <= ogr) {
-            if(a[k].first < nm) {
-                nm = a[k].first;
-                r = k + 1;
-            }
+    double ogr = 1.0 * nm / 3 + 1.0 * ng / 3 * 2;
+
+    for(int k = 0; k < n; k++)
+        if(a[k] <= ogr){
+            cout << k + 1;
+            return 0;
         }
-        if(k == n - 1) {
-            h[1].second = r;
-        }
-    }
-    cout << h[1].second;
 
     return 0;
 }
